e87f6859a67fe83fdb325be1a1ffa0a195474364
Kevin Manceau <kevin.manceau@gmail.com>
2025-01-02 15:38:52 +0100

    created some routes and design the nav bar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'momo_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/upload', name: 'upload')]
+    public function upload(): Response
+    {
+        return $this->render('main/upload.html.twig', [
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', [
+        ]);
+    }
+
+    #[Route('/acount_edit', name: 'acount_edit')]
+    public function acount_edit(): Response
+    {
+        return $this->render('main/acount_edit.html.twig', [
         ]);
     }
 }
