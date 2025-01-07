e372acd3e520b2afdc2ec15688603b147bd74639
Kevin Manceau <kevin.manceau@gmail.com>
2025-01-07 09:53:45 +0100

    designed admin home

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_index')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+        ]);
+    }
+}
